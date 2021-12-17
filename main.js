7284b9d5d364ae48dbced0797eb531aa3db85d3b
samiryakhlef <samir.yakhlef@gmail.com>
2021-12-17 15:29:03 +0100

    exerciceJS

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/* exercice 1
+const a = 20;
+for (let i = 20; i >= 0; i -= 2) {
+console.log(i);
+}*/
+
+// exercice 2
+function premier(nbr) {
+    for (let i = 2; i < nbr; i++) {
+        if (nbr % i === 0) {
+            return false;
+        }
+    }
+    console.log(nbr);
+    return nbr;
+}
+for (let nbr = 1; nbr <= 100; nbr++) {
+    premier(nbr);
+}
+
+// exercice 3
