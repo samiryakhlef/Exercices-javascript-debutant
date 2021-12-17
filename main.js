f85254717aacbe8d4f1d897f2aa52fe6a9e29c6b
samiryakhlef <samir.yakhlef@gmail.com>
2021-12-17 16:27:08 +0100

    exJS

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const a = 20;
 for (let i = 20; i >= 0; i -= 2) {
 console.log(i);
-}*/
+}
 
 // exercice 2
 function premier(nbr) {
@@ -19,3 +19,17 @@ for (let nbr = 1; nbr <= 100; nbr++) {
 }
 
 // exercice 3
+const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < 10; i++) {
+tab.length[i];
+console.log(tab);
+}*/
+var result = '';
+for (var i = 1; i < 11; i++) {
+  for (var j = 1; j < 11; j++) {
+    result += ' '.repeat(3 - (i * j).toString().length) + (i * j) + ' ';
+  }
+  result += '\n';
+}
+console.table(result);
